8c9584618a399943fd45ff6daec1cabfed7d6daa
C0C25062 <c0c2506244@edu.teu.ac.jp>
2026-06-16 14:27:38 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -16,10 +17,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")  
+    #こうかとん初期化 
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    #爆弾の初期化
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #半径10の赤い円の描画
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect() #爆弾rect
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT) #横・縦初期座標
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -46,6 +56,9 @@ def main():
         
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
